f505c9aa04c7d41a285132cefbcb1290ec5cd083
simple40 <spvighnesh99@gmail.com>
2023-02-12 20:39:30 +0530

    docker done

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.jvm.tasks.Jar
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -15,9 +16,16 @@ application {
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
+ktor{
+
+    fatJar {
+        archiveFileName.set("Ktor-Sample.jar")
+    }
+}
 
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
@@ -26,4 +34,23 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}"
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Implementation-Title"] = "Gradle Jar File Example"
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain"
+    }
+    from(configurations.runtimeClasspath.get().map({ if (it.isDirectory) it else zipTree(it) }))
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
